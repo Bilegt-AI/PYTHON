622265fa0043cddfb527bcc9060def424f5bf11b
Bilegt-AI <bilegted0810@gmail.com>
2026-06-20 17:09:01 +0800

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,46 @@
+#STRING.
+
+first_name="Bro"
+food="pizza"
+email="bilegted0810@gmail.com"
+
+    #print(f"Hello {first_name}")
+    #print(f"You like {food}")
+    #print(f"Your Email Is {email}")
+
+#INTEGERS. ( hole number )
+
+age=25
+quantity=3
+num_of_students=30
+
+    #print(f"You Are {age} Years Old")
+    #print(f" You Are Buying {quantity} items")
+    #print(f" Your Class has {num_of_students} Students")
+
+#FLOAT.
+
+price=10.99
+gpa=3.2
+distance=5.5
+
+    #print(f"The price is ${price}")
+    #print(f" your gpa is {gpa}")
+    #print(f"youy ran {distance} km")
+
+#BOOLEAN.
+
+is_student= False
+for_sale= False
+
+    #print(f"are you student?: {is_student}")
+
+    #if is_student:
+        #print(" You Are a Student")
+    #else:
+        #print(" You Are NOT a Student")
+
+if for_sale:
+    print(" That item is for sale ")
+else:
+    print("that item is not available")
