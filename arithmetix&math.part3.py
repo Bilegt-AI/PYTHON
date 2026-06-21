66a1273c6320ae1ce866a5135b802716a9fc682f
Bilegt-AI <bilegted0810@gmail.com>
2026-06-21 10:13:51 +0800

    Create arithmetix&math.part3.py

---
+++ arithmetix&math.part3.py
@@ -0,0 +1,9 @@
+import math
+    #print(math.pi)
+    #print(math.e)
+
+x=9.9
+#result=math.sqrt(x). ( sqrt нь язгуур зэргийг тодорхойлно 9-ын язгуур нь 3 болно)
+#result=math.ceil(x). ( функц нь тоог үргэлж дээш хамгийн ойрхон бүхэл тоо руу дугуйлдаг)
+#result=math.floor(x).  (ceil-ын яг эсрэг)
+print(result)
