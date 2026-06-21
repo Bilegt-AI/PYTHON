9b8cde0d7c76783144ff4836609c86f300274116
Bilegt-AI <bilegted0810@gmail.com>
2026-06-22 07:15:43 +0800

    Create operators.part1.py

---
+++ operators.part1.py
@@ -0,0 +1,10 @@
+# logical operators.
+# or (энэ нь нэг утга нь зөв байгаад бусад нь буруу байсан ч нэг нь л 
+# зөв байвал print хийнэ.)
+
+temp=-5
+is_raining=True
+if temp>35 or temp <0 or is_raining:
+    print("The outdoor event is cancelled")
+else:
+    print("The outdoor event is still scheduled") 
