cd0ff253c03408c0ff9b137ac6e4209f2bf06259
Bilegt-AI <bilegted0810@gmail.com>
2026-06-21 08:26:32 +0800

    Create type casting.py

---
+++ type casting.py
@@ -0,0 +1,19 @@
+name="Bilegt"
+age=16
+gpa=4.9
+is_student=True
+
+    #print(type(is_student)) хувьсагчийн төрлөө мэдэхгүй бол үүгээр мэдэж болно.
+
+        #gpa=int(gpa) бутархай тоог бүхэл тоо болгох
+        #print(gpa)
+
+            #age=float(age) бүхэл тоог бутархай тоо болгох
+            #print(age)
+
+                #age=str(age)
+                #age+="1"
+                #print(age)
+
+name=bool(name)
+print(name)
