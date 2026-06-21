857151489e2264ebdcf7968b56388538b2c5ced1
Bilegt-AI <bilegted0810@gmail.com>
2026-06-21 10:51:48 +0800

    Create %exercise 2.py

---
+++ %exercise 2.py
@@ -0,0 +1,6 @@
+mongolia=int(input("Та өөрийн монгол хэлний дүнгээ оруулаарай! "))
+english=int(input("Та өөрийн англи хэлний дүнгээ оруулаарай! "))
+math=int(input("Та өөрийн математикийн дүнгээ оруулаарай! "))
+
+print(mongolia+english+math)
+print((mongolia+english+math)/3)
