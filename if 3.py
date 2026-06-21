0899d03dcf04f1620f7624b76005985110783b5b
Bilegt-AI <bilegted0810@gmail.com>
2026-06-22 05:38:45 +0800

    Create if 3.py

---
+++ if 3.py
@@ -0,0 +1,8 @@
+num=int(input("Give me first number"))
+num_2=int(input("Give me second number"))
+if num>num_2:
+    print("Эхний тоо хоёр дахь тооноос илүү их")
+elif num<num_2:
+    print("Хоёр дахь тоо эхний тооноос илүү их")
+else:
+    print("Хоёр тоо тэнцүү")
