0bf8344c538b435559c3f106e5f0506c16ee800e
Bilegt-AI <bilegted0810@gmail.com>
2026-06-21 11:07:27 +0800

    Create %exercise 3.py

---
+++ %exercise 3.py
@@ -0,0 +1,14 @@
+#Arithmetic operators.
+
+input("Та доор өөрийн дурын тоог оруулаарай!")
+first=float(input("Таны эхний тоо"))
+second=float(input("Таны хоёрдугаарх тоо"))
+
+print(first+second)
+print(first-second)
+print(first*second)
+print(first/second)
+
+print(first//second) #бүхэл хуваах
+print(first%second) # үлдэгдэл
+print(first**second) #зэрэг
