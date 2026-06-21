284d7260905d0e51f68a99daa77c607cd4603c29
Bilegt-AI <bilegted0810@gmail.com>
2026-06-21 11:43:42 +0800

    Create %exercise 5.py

---
+++ %exercise 5.py
@@ -0,0 +1,7 @@
+import math
+
+print("Та тэгш өнцөгт гурвалжны хоёр хөлийн уртыг олоорой!")
+first=int(input("Эхний тоо: "))
+second=int(input("Эхний тоо: "))
+
+print(math.sqrt(pow(first, 2)+pow(second, 2)))
