03eee9a58d2ed7f8726bf385b1ccad6a8ac5c166
Bilegt-AI <bilegted0810@gmail.com>
2026-06-22 05:59:11 +0800

    Create if 5.py

---
+++ if 5.py
@@ -0,0 +1,10 @@
+first=str(input("Name"))
+second=int(input("Password"))
+if first=="Bilegt":
+    print("Correct!")
+else:
+    print("Incorret!")
+if second==1234:
+    print("Correct")
+else:
+    print("Incorrect")
