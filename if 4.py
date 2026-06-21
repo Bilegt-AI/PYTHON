01ca871b0cc2c749b513f96d12ab428b0ace41bf
Bilegt-AI <bilegted0810@gmail.com>
2026-06-22 05:51:17 +0800

    Create if 4.py

---
+++ if 4.py
@@ -0,0 +1,11 @@
+# насны ангилал.
+
+age=int(input("Enter your age!"))
+if age<=12:
+    print(f"{age} бол та ХҮҮХЭД")
+elif 13 <= age <= 19:
+    print(f"{age} бол та ӨСВӨР ҮЕ")
+elif 20<=age<=59:
+    print(f"{age} бол та НАСАНД ХҮРЭГЧ")
+else:
+    print("Ахмад настан")
