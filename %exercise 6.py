5d29ae7d05c3ca884a3346dc754c70f3c25354c5
Bilegt-AI <bilegted0810@gmail.com>
2026-06-21 11:55:23 +0800

    Create %exercise 6.py

---
+++ %exercise 6.py
@@ -0,0 +1,12 @@
+import math
+
+print("Та өөрийн хүссэн бутархай тоогоо оруулаарай!")
+code=float(input("Яг энд тоогоо оруулаарай🥰"))
+
+result=round(code)
+result2=math.ceil(code)
+result3=math.floor(code)
+
+print(result)
+print(result2)
+print(result3)
