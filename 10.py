056d5177fb79b09a881b6eac8e64a25d7c24840b
Bilegt-AI <bilegted0810@gmail.com>
2026-06-22 04:33:15 +0800

    Create 10.py

---
+++ 10.py
@@ -0,0 +1,13 @@
+first=print("Барааны нэр: ")
+second=print("Үнэ: ")
+third=print("Тоо хэмжээ: ")
+
+first=input("Нэр: ")
+second=float(input("Үнэ: "))
+third=int(input("Тоо хэмжээ: "))
+
+total=second*third
+if total>=50:
+    print("Хямдрал авах боломжиой: ")
+else:
+    print("Хямдрал байхгүй: ")
