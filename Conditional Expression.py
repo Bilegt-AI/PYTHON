e15485cb49bf72955363644aa4194cd56f68b42f
Bilegt-AI <bilegted0810@gmail.com>
2026-06-22 14:08:09 +0800

    Create Conditional Expression.py

---
+++ Conditional Expression.py
@@ -0,0 +1,15 @@
+#Conditional Expression.
+# X if condition else Y.
+num=5
+a=6
+b=7
+age=13
+temperature=20
+
+print("Positive" if num>0 else "Negative")
+result="EVEN" if num%2==0 else "ODD"
+max_num= a if a>b else b
+min_num= a if a<b else b
+status="Adult" if age>=18 else "Child"
+weather="HOT" if temperature >20 else "COLD"
+print(weather)
