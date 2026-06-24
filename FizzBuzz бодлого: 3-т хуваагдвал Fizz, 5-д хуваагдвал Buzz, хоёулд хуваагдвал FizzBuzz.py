032cdd145b3b2bc5fda54a13b0ffa18331defc6d
Bilegt-AI <bilegted0810@gmail.com>
2026-06-24 18:26:30 +0800

    Create FizzBuzz бодлого: 3-т хуваагдвал Fizz, 5-д хуваагдвал Buzz, хоёулд хуваагдвал FizzBuzz.py

---
+++ FizzBuzz бодлого: 3-т хуваагдвал Fizz, 5-д хуваагдвал Buzz, хоёулд хуваагдвал FizzBuzz.py
@@ -0,0 +1,11 @@
+# Бодлого 9: FizzBuzz
+n = int(input("Тоо оруул: "))
+
+if n % 3 == 0 and n % 5 == 0:
+    print("FizzBuzz")
+elif n % 3 == 0:
+    print("Fizz")
+elif n % 5 == 0:
+    print("Buzz")
+else:
+    print(n)
