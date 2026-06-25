496808b1cc89ed15dc1636f03570f069d94c0bb4
Bilegt-AI <bilegted0810@gmail.com>
2026-06-25 23:08:24 +0800

    Create arithmetic operators7.py

---
+++ arithmetic operators7.py
@@ -0,0 +1,11 @@
+# Имэйл хаягийг задлах.
+
+gmail=input("Enter Your Email: ")
+first=gmail.find("@")
+second=gmail[: first]
+third=gmail[: first+1]
+forth=gmail.upper()
+print(first)
+print(second)
+print(third)
+print(forth)
