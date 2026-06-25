52ee099e72b977ae4c4a62c7f3efc024d617f52a
Bilegt-AI <bilegted0810@gmail.com>
2026-06-25 22:59:23 +0800

    Create arithmetic operators6.py

---
+++ arithmetic operators6.py
@@ -0,0 +1,8 @@
+# нийлмэл нэр ба утасны дугаар.
+
+num=input("Enter Your Phone Number: ")
+name=input("Enter Your Full Name: ")
+first=name.upper()
+num=num.isdigit()
+result=len(name)
+print(f"Name {name}, Number{num}")
