8efd18eeb6fcd31989a05efa518eb7ca4cb1e69d
Bilegt-AI <bilegted0810@gmail.com>
2026-07-01 16:42:10 +0800

    Create nested loop12.py

---
+++ nested loop12.py
@@ -0,0 +1,15 @@
+# 12-th Exersice.
+
+# 2. Тоон гурвалжин
+# Мөр бүрт мөрийн дугаараа давтаж хэвлэ:
+
+# 1
+# 2 2
+# 3 3 3
+# 4 4 4 4
+
+total=1
+for i in range(4):
+    for b in range(i*1+1):
+        print(i+1,end="")
+    print()
