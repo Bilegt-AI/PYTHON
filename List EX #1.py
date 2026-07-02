38f6ed05976874ddf262a32bcbe868e9bfd7ebee
Bilegt-AI <bilegted0810@gmail.com>
2026-07-02 11:52:07 +0800

    Create List EX #1.py

---
+++ List EX #1.py
@@ -0,0 +1,13 @@
+# Даалгавар 1: Anh list үүсгэх
+
+# Дараах зүйлсийг хэвлэ (print):
+
+# a) list-ийн урт (length) хэд вэ?
+# b) "cat" энэ list дотор байгаа эсэхийг шалга (True/False гаргах)
+# c) "lion" list дотор хэддүгээр index дээр байгааг ол
+
+animals = ["dog", "cat", "elephant", "lion", "tiger"]
+
+print(len(animals))
+print("cat" in animals)
+print(animals.index("lion"))
