fdf173eb4165c574f5d022a2e260c868cf765176
Bilegt-AI <bilegted0810@gmail.com>
2026-07-02 11:52:48 +0800

    Create List Intro.py

---
+++ List Intro.py
@@ -0,0 +1,40 @@
+# collection = single "variable" used to store multiple values
+#   List  = [] ordered and changeable. Duplicates OK
+#   Set   = {} unordered and immutable, but Add/Remove OK. NO duplicates
+#   Tuple = () ordered and unchangeable. Duplicates OK. FASTER
+
+fruits=["apple","orange","banana","coconut"]
+
+# print(dir(fruits))
+# print(help(fruits))
+# print(len(fruits))
+# print("pineapple" in fruits)
+
+# fruits[1]="pineapple"
+# for fruit in fruits:
+    #print(fruit)
+
+# fruits.append("pineapple")
+# print(fruits)
+
+# fruits.remove("apple")
+# print(fruits)
+
+# fruits.insert(0,"pineapple")
+# print(fruits)
+
+# fruits.sort() # үсэг байвал үсгийн дарааллаар эрэмбэлнэ тоо байвал багаас нь ихрүү эрэмбэлнэ.
+# print(fruits)
+
+# fruits.reverse()
+# rint(fruits)
+
+# fruits.clear() # list дотор байгаа бүх element-ыг арилгана.
+# print(fruits)
+
+# print(fruits.index("coconut")) # coconut list-ын яг хэд дэх дараалалд байгааг хэвлэнэ.
+# print(fruits.count("banana"))
+
+#print(fruits[::-1])
+#for fruit in fruits:
+    #print(fruit)
