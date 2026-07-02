d226621c7bdea94cf4c0f346ed349293cce45bf8
Bilegt-AI <bilegted0810@gmail.com>
2026-07-02 13:19:41 +0800

    Create List #4.py

---
+++ List #4.py
@@ -0,0 +1,11 @@
+# Даалгавар 4: Тоолох
+
+# a) 85 тоо энэ list дотор хэдэн удаа давтагдаж байгааг ол (count ашиглан)
+# b) 90 тооны эхний index-ийг ол (index ашиглан)
+# c) list-ийн урт хэд вэ?
+
+scores = [85, 90, 85, 70, 85, 60, 90]
+
+print(scores.count(85))
+print(scores.index(90))
+print(len(scores))
