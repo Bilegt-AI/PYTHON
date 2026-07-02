6e01affe1389d407f8603a5a2ccbc0b5c2d8932e
Bilegt-AI <bilegted0810@gmail.com>
2026-07-02 13:28:43 +0800

    Create List EX#5.py

---
+++ List EX#5.py
@@ -0,0 +1,16 @@
+# Даалгавар 5: For loop-той хослуулах
+
+# a) for loop ашиглан бүх нэрийг нэг нэгээр нь хэвлэ
+# b) for loop ашиглан хэн нэрэнд нь "a" үсэг байгааг олж хэвлэ 
+# (tip: if "a" in student:)
+
+students = ["Bat", "Sara", "Tuya", "Erdene"]
+
+for b in students:
+    print(b)
+
+
+students = ["Bat", "Sara", "Tuya", "Erdene"]
+for b in students:
+    if "a" in b:
+        print(b)
